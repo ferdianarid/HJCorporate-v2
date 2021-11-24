99c3f2a10288fc4ae9c0e5d9e09cd3e0a88a92ec
Fajar Aji Pamungkas <84966261+fapamungkas@users.noreply.github.com>
2021-11-24 16:00:53 +0700

    update section about

---
+++ components/Twoacc.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="twoacc flex ml-80">
+    <div class="flex items-center">
+      <img class="pl-7" src="../static/images/iconGraph.svg" alt="" />
+    </div>
+    <div class="flex flex-col justify-start pl-4">
+      <h3 class="font-semibold text-3xl pt-6">10 +</h3>
+      <h3 class="font-medium">Tahun Berpengalaman</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  nama: "Twoacc",
+};
+</script>
+
+<style>
+.twoacc {
+  margin-top: -60px;
+  width: 345px;
+  height: 114px;
+  background: rgba(255, 255, 255, 0.7);
+  box-shadow: 0px 30px 60px rgba(32, 56, 85, 0.15);
+  backdrop-filter: blur(16px);
+  border-radius: 8px;
+  z-index: 2;
+  position: absolute;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,39 @@
 <template>
-       <div class="main px-32">
-              <Footer />
-       </div>
+  <div class="main px-32">
+    <div class="flex items-center">
+      <h6 class="text-lg font-sans font-medium pb-4">Tentang Kami</h6>
+      &nbsp;&nbsp;
+      <hr width="100px" color="white" class="-mt-3" />
+    </div>
+    <div>
+      <h3 class="text-5xl pt-1.5 pb-16">
+        Kami selalu mengedepankan kepuasan para partner kami demi menghasilkan
+        sebuah produk yang diinginkan
+      </h3>
+      <Twoacc />
+      <img class="pb-6" src="../static/images/maskImage.svg" alt="" />
+    </div>
+    <Accessories />
+    <Footer />
+  </div>
 </template>
 
 <script>
-export default {}
+import Twoacc from "~/components/Twoacc.vue";
+export default {
+  components: { Twoacc },
+};
 </script>
 
 <style>
 * {
-       font-family: "Poppins", sans-serif;
-       box-sizing: border-box;
-       margin: 0;
-       scroll-behavior: smooth;
+  font-family: "Poppins", sans-serif;
+  box-sizing: border-box;
+  margin: 0;
+  scroll-behavior: smooth;
 }
 
 .main {
-       max-width: 1440px;
+  max-width: 1440px;
 }
 </style>
