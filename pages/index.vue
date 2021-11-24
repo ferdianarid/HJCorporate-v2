830cf3c9abdb25d438d00713bca868c133fd84fd
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-11-24 16:53:42 +0700

    resolve conflict

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,24 @@
 <template>
-<<<<<<< HEAD
-       <div class="main px-4 md:px-32">
-              <Footer />
-       </div>
-=======
-  <div class="main px-32">
-    <div class="flex items-center">
-      <h6 class="text-lg font-sans font-medium pb-4">Tentang Kami</h6>
-      &nbsp;&nbsp;
-      <hr width="100px" color="white" class="-mt-3" />
-    </div>
-    <div>
-      <h3 class="text-5xl pt-1.5 pb-16">
-        Kami selalu mengedepankan kepuasan para partner kami demi menghasilkan
-        sebuah produk yang diinginkan
-      </h3>
-      <Twoacc />
-      <img class="pb-6" src="../static/images/maskImage.svg" alt="" />
-    </div>
-    <Accessories />
-    <Footer />
-  </div>
->>>>>>> 99c3f2a10288fc4ae9c0e5d9e09cd3e0a88a92ec
+	<div>
+		<div class="main px-4 md:px-32">
+			<Footer />
+		</div>
+	</div>
 </template>
 
 <script>
-import Twoacc from "~/components/Twoacc.vue";
-export default {
-  components: { Twoacc },
-};
+export default {};
 </script>
 
 <style>
 * {
-  font-family: "Poppins", sans-serif;
-  box-sizing: border-box;
-  margin: 0;
-  scroll-behavior: smooth;
+	font-family: "Poppins", sans-serif;
+	box-sizing: border-box;
+	margin: 0;
+	scroll-behavior: smooth;
 }
 
 .main {
-  max-width: 1440px;
+	max-width: 1440px;
 }
 </style>
