2a70057d971430b0c8c580a961b5c74044ac8167
Fajar Aji Pamungkas <84966261+fapamungkas@users.noreply.github.com>
2021-11-25 10:29:05 +0700

    Add a benefit card component

---
+++ components/CardBenefit.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="cardBenefit tracking-wide mb-14 border border-black-300">
+    <h5 class="text-2xl font-semibold pb-2 pl-7">
+      Komunikasi yang jelas <br />
+      dan mudah
+    </h5>
+    <p class="text-partner text-base font-regular pl-7">
+      Partner akan dimudahkan dengan <br />
+      sistem komunikasi kami yang <br />
+      tersedia untuk kapan saja
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardBenefit",
+};
+</script>
+
+<style>
+.cardBenefit {
+  width: 379px;
+  height: 154px;
+}
+
+.text-partner {
+  color: #404852;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="main px-32">
+    <!-- Benefit -->
+    <div>
+      <div class="flex items-center">
+        <h6 class="text-lg font-sans font-medium pb-4">
+          Keuntungan Partner Kami
+        </h6>
+        &nbsp;&nbsp;
+        <hr width="100px" color="white" class="-mt-3" />
+      </div>
+      <div>
+        <h3 class="text-5xl pt-1.5 pb-16 tracking-wide">
+          Keuntungan menjadi partner <br />
+          kerja pada HJ Corporate
+        </h3>
+      </div>
+      <CardBenefit />
+    </div>
+    <!-- About Us -->
     <div class="flex items-center">
       <h6 class="text-lg font-sans font-medium pb-4">Tentang Kami</h6>
       &nbsp;&nbsp;
       <hr width="100px" color="white" class="-mt-3" />
     </div>
     <div>
-      <h3 class="text-5xl pt-1.5 pb-16">
+      <h3 class="text-5xl pt-1.5 pb-16 tracking-wide">
         Kami selalu mengedepankan kepuasan para partner kami demi menghasilkan
         sebuah produk yang diinginkan
       </h3>
