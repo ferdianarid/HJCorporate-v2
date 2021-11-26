3c821655952846009a61d24f930f39d103fde0be
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-11-26 13:58:57 +0700

    update portfolio

--- pages/index.vue
+++ pages/index.vue
@@ -1,99 +1,60 @@
 <template>
 	<div>
 		<div class="main md:px-32 px-4 m-auto">
-			<!-- Navbar -->
-			<Navbar />
-			<!-- Benefit -->
+		<!-- Navbar -->
+		<Navbar />
+		<Portfolio />
+		<!-- Benefit -->
+		<div>
+			<div class="flex items-center">
+				<h6 class="text-lg font-sans font-medium pb-4">
+					Keuntungan Partner Kami
+				</h6>
+				&nbsp;&nbsp;
+				<hr width="100px" color="white" class="-mt-3" />
+			</div>
 			<div>
-				<div class="flex items-center">
-					<h6 class="text-lg font-sans font-medium pb-4">
-						Keuntungan Partner Kami
-					</h6>
-					&nbsp;&nbsp;
-					<hr width="100px" color="white" class="-mt-3" />
-				</div>
-				<div>
-					<h3 class="text-4xl pt-1.5 pb-16 font-medium">
-						Keuntungan menjadi partner <br />
-						kerja pada HJ Corporate
-					</h3>
-				</div>
-				<div class="flex md:flex-row flex-col items-center">
-					<CardBenefit />
-					<CardBenefit />
-					<CardBenefit />
-				</div>
+				<h3 class="text-4xl pt-1.5 pb-2 font-medium">
+					Keuntungan menjadi partner kerja pada HJ Corporate.
+				</h3>
+				<p class="pb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi commodi quaerat ullam.</p>
 			</div>
-			<!-- About Us -->
-			<div class="py-10">
-				<div class="flex items-center">
-					<h6 class="text-lg font-sans font-medium pb-4">Tentang Kami</h6>
-					&nbsp;&nbsp;
-					<hr width="100px" color="white" class="-mt-3" />
-				</div>
-				<div>
-					<h3 class="text-4xl pt-1.5 pb-16 font-medium">
-						Kami selalu mengedepankan kepuasan para partner kami demi menghasilkan
-						sebuah produk yang diinginkan
-					</h3>
-					<Glass />
-					<img class="pb-6" src="../static/images/maskImage.svg" alt="" />
-				</div>
-				<SecondGlass />
+			<div class="flex md:flex-row flex-col items-center">
+				<CardBenefit />
+				<CardBenefit />
+				<CardBenefit />
 			</div>
-			<Footer />
+		</div>
+		<!-- About Us -->
+		<div class="py-10">
+			<div class="flex items-center">
+				<h6 class="text-lg font-sans font-medium pb-4">Tentang Kami</h6>
+				&nbsp;&nbsp;
+				<hr width="100px" color="white" class="-mt-3" />
+			</div>
+			<div>
+				<h3 class="text-4xl pt-1.5 pb-16 font-medium">
+					Kami selalu mengedepankan kepuasan para partner kami demi menghasilkan
+					sebuah produk yang diinginkan
+				</h3>
+				<Glass />
+				<img class="pb-6" src="../static/images/maskImage.svg" alt="" />
+			</div>
+			<SecondGlass />
+		</div>
+		<Footer />
 		</div>
 	</div>
-<<<<<<< HEAD
-=======
-=======
-  <div class="main px-32">
-    <Portfolio />
-    <!-- Benefit -->
-    <div>
-      <div class="flex items-center">
-        <h6 class="text-lg font-sans font-medium pb-4">
-          Keuntungan Partner Kami
-        </h6>
-        &nbsp;&nbsp;
-        <hr width="100px" color="white" class="-mt-3" />
-      </div>
-      <div>
-        <h3 class="text-5xl pt-1.5 pb-16 tracking-wide">
-          Keuntungan menjadi partner <br />
-          kerja pada HJ Corporate
-        </h3>
-      </div>
-      <CardBenefit />
-    </div>
-    <!-- About Us -->
-    <div class="flex items-center">
-      <h6 class="text-lg font-sans font-medium pb-4">Tentang Kami</h6>
-      &nbsp;&nbsp;
-      <hr width="100px" color="white" class="-mt-3" />
-    </div>
-    <div>
-      <h3 class="text-5xl pt-1.5 pb-16 tracking-wide">
-        Kami selalu mengedepankan kepuasan para partner kami demi menghasilkan
-        sebuah produk yang diinginkan
-      </h3>
-      <Twoacc />
-      <img class="pb-6" src="../static/images/maskImage.svg" alt="" />
-    </div>
-    <Accessories />
-    <Footer />
-  </div>
->>>>>>> d66d8a3bcfff389f2ff9312a91dccfbf563e3341
->>>>>>> 7851891fd93040b91062e829a31a8ff635f57ef8
 </template>
-
 <script>
 import Navbar from "../components/Navbar.vue"
-import SecondGlass from "~/components/SecondGlass.vue";
+import SecondGlass from "~/components/SecondGlass.vue"
+import Portfolio from "~/components/Portfolio.vue";
 export default {
 	components: {
 		Navbar,
-		SecondGlass
+		SecondGlass,
+		Portfolio
 	}
 };
 </script>
