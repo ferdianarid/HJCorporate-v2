1d7cb92fcd649f2adfb8c7824e8d492ff5ea0caa
Fajar Aji Pamungkas <84966261+fapamungkas@users.noreply.github.com>
2021-12-06 13:06:01 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            outline: {
-                blue: ['2px solid #1189B8', '1px'],
-            },
-            colors: {
-                primary: '#1189B8',
-                secondary: '#232323'
-            }
-        },
+  mode: "jit",
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      outline: {
+        blue: ["2px solid #1189B8", "1px"],
+      },
+      colors: {
+        primary: "#1189B8",
+        secondary: "#232323",
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
-}
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
