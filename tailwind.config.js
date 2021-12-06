b8969a34b903deb5a6aa4137d34774903d331eda
Fajar Aji Pamungkas <84966261+fapamungkas@users.noreply.github.com>
2021-12-06 13:22:47 +0700

    ss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,29 @@
 module.exports = {
-    mode: 'jit',
-    purge: [
-        './public/**/*.html',
-        './src/**/*.{js,jsx,ts,tsx,vue}'
-    ],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            outline: {
-                blue: ['2px solid #1189B8'],
-            },
-            outline: {
-                  blue: ["2px solid #1189B8", "1px"],
-                },
-                colors: {
-                  primary: "#1189B8",
-                  secondary: "#232323",
-                },
-            colors: {
-                primary: '#1189B8',
-                primaryGreen: '#187C79',
-                secondary: '# 232323 ',
-                techColor: '#2D8DFE',
-                designColor: '#E72634',
-                consultantColor: '#B426DC',
-                farmColor: '#55BEDC'
-            },
-        }
-    }
-      
+  mode: "jit",
+  purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      outline: {
+        blue: ["2px solid #1189B8"],
+      },
+      outline: {
+        blue: ["2px solid #1189B8", "1px"],
+      },
+      colors: {
+        primary: "#1189B8",
+        secondary: "#232323",
+      },
+      colors: {
+        primary: "#1189B8",
+        primaryGreen: "#187C79",
+        secondary: "# 232323 ",
+        techColor: "#2D8DFE",
+        designColor: "#E72634",
+        consultantColor: "#B426DC",
+        farmColor: "#55BEDC",
+      },
+    },
+  },
   plugins: [],
 };
