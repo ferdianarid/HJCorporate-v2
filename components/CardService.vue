6be9e84dc9376b879b26765f078b361bc27b1843
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-12-09 23:46:03 +0700

    update

--- components/CardService.vue
+++ components/CardService.vue
@@ -1,44 +1,34 @@
 <template lang="">
   <div>
-    <h3 class="text-4xl font-medium pt-1.5 pb-6">
-      Siap berkolaborasi bersama kami?
-    </h3>
-    <p class="text-base pb-10">
-      Jika anda sudah memiliki ide dan konsep mengenai projek apa yang akan
-      dikerjakan maka, dapat mengisikan formulir
-    </p>
-    <div class="child text-left pb-14">
-      <div class="bg-white p-12 md:p-14 border-black border-t-4">
-        <img class="pb-8" src="../static/images/phone.svg" alt="" />
-        <h4 class="pb-8 text-4xl font-medium">
-          Belum memiliki ide untuk projek anda?
-        </h4>
-        <button
-          class="
-            py-2
-            px-6
-            outline-blue
-            rounded
-            font-medium
-            text-primary
-            hover:text-white hover:bg-primary
-            mx-1
-          "
-        >
-          Jadwalkan Meeting
-        </button>
-      </div>
-    </div>
-  </div>
-       <div>
+              <h3 class="text-4xl font-medium pt-1.5 pb-6">
+              Siap berkolaborasi bersama kami?
+              </h3>
+              <p class="text-base pb-10">
+              Jika anda sudah memiliki ide dan konsep mengenai projek apa yang akan
+              dikerjakan maka, dapat mengisikan formulir
+              </p>
               <div class="child text-left pb-14">
                      <div class="bg-white p-12 md:p-14 border-black border-t-4">
-                     <img class="pb-8" src="../static/images/phone.svg" alt="" />
-                     <h4 class="pb-8 text-4xl font-medium">Belum memiliki ide untuk projek anda?</h4>
-                     <p class="pb-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit magni incidunt natus</p>
-                     <button class="py-2 px-6 outline-blue rounded font-medium text-primary hover:text-white  hover:bg-primary mx-1">Jadwalkan Meeting</button>
+                            <img class="pb-8" src="../static/images/phone.svg" alt="" />
+                            <h4 class="pb-8 text-4xl font-medium">
+                            Belum memiliki ide untuk projek anda?
+                            </h4>
+                            <button
+                            class="
+                            py-2
+                            px-6
+                            outline-blue
+                            rounded
+                            font-medium
+                            text-primary
+                            hover:text-white hover:bg-primary
+                            mx-1
+                            "
+                            >
+                            Jadwalkan Meeting
+                            </button>
                      </div>
-              </div>
+                     </div>
        </div>
 </template>
 <script>
